043feb09b688fba6df28e1103e9d67f9dcc9282b
mammadlinurlan <nurlanym@code.edu.az>
2021-12-01 11:24:46 +0400

    11:24 , commit time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,24 @@ $(document).ready(function () {
     let lastIcon;
     let lastTitle
     let lastDescsId;
+    let no1 = document.querySelector(`div[data-id="1"]`).nextElementSibling
+    $(no1).slideDown()
+    lastDesc = no1
+    lastDescsId = lastDesc.id
+
+    let no1Title = document.querySelector(`div[data-id="1"]`)
+    $(no1Title).toggleClass(`active`)
+     lastTitle  = no1Title 
+    
+
+    let no1Icon = document.querySelector(`div[data-id="1"]>div.icon`)
+    $(no1Icon).toggleClass('customAnimate')
+    lastIcon = no1Icon
+    
     $(".title").click(function () {
         let dataid = this.getAttribute("data-id");
 
-
-
-
-
-        if (lastDescsId == dataid) {
-            console.log("test");
-
-        } else {
+        if (lastDescsId != dataid) {
             lastDesc ? lastDesc.style.display = "none" : null;
             lastTitle ? lastTitle.classList.remove("active") : null;
             lastIcon ? lastIcon.classList.remove("customAnimate") : null;
@@ -22,7 +29,6 @@ $(document).ready(function () {
         let icon = document.querySelector(`div[data-id="${dataid}"]>div.icon`)
         $(icon).toggleClass('customAnimate')
         lastIcon = icon
-
 
 
         $(this).toggleClass('active')
